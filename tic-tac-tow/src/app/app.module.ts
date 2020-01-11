fe1219b6f0ceb700b58ad457c259ce24b917bc17
abdellah <abdel.ysf@gmail.com>
2020-01-11 12:04:27 +0100

    updating angular version

--- tic-tac-tow/src/app/app.module.ts
+++ tic-tac-tow/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './appConponent/app.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
